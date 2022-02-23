1aea257f50e05c809f04e1620dd2b39f4f09cabb
naca-nyan <naca.nyan@gmail.com>
2022-02-24 00:03:20 +0900

    Implement client

---
+++ public/server.py
@@ -0,0 +1,33 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+from pythonosc import udp_client
+import json
+import webbrowser
+
+ip = '127.0.0.1'
+port = 9090
+
+vrcClientIp = '127.0.0.1'
+vrcClientPort = 9000
+client = udp_client.SimpleUDPClient(vrcClientIp, vrcClientPort)
+
+
+class PostHandler(SimpleHTTPRequestHandler):
+    def do_POST(self):
+        content_len = int(self.headers['content-length'])
+        post_body = self.rfile.read(content_len).decode('utf8')
+        print(post_body)
+        data = json.loads(post_body)
+        self.send_response(200)
+        if "path" in data and "value" in data:
+            path = data["path"]
+            value = data["value"]
+            client.send_message(path, value)
+
+
+PostHandler.extensions_map['.js'] = 'text/javascript'
+
+if __name__ == '__main__':
+    print(f"Staring the server at http://{ip}:{port}")
+    webbrowser.open("http://localhost:9090")
+    server = HTTPServer((ip, port), PostHandler)
+    server.serve_forever()
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,63 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+
+interface ConnectionState {
+  state: "not connected" | "connecting" | "connected";
+}
+
+const state = ref<ConnectionState>({ state: "not connected" });
+
+const serverAddr = ref("ws://");
+const parameter = ref("/avatar/parameters/hoge");
+const value = ref("1");
+
+let _sock: WebSocket;
+
+function connect() {
+  const addr = serverAddr.value;
+  const sock = new WebSocket(addr);
+  _sock = sock;
+  sock.addEventListener("open", () => {
+    state.value.state = "connected";
+  });
+  sock.addEventListener("message", async (message) => {
+    const data = await JSON.parse(message.data);
+    const { path, value } = data;
+    fetch("http://localhost:9090/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        path: path,
+        value: value,
+      }),
+    });
+  });
+}
+
+function send() {
+  _sock.send(
+    JSON.stringify({
+      path: parameter,
+      value: value,
+    })
+  );
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div v-if="state.state === 'connected'">
+    <label for="parameter">parameter</label>
+    <input v-model="parameter" />
+    <label for="value">value</label>
+    <input v-model="value" />
+    <button @click="send">send</button>
+  </div>
+  <div v-else>
+    <input type="text" v-model="serverAddr" />
+    <button @click="connect">connect</button>
+  </div>
 </template>
 
 <style>
